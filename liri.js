9c5bf124dcc234d8f0dde88f2cf7b8e26fc0ca0f
Aaron Lee <worldedit05@gmail.com>
2016-10-14 14:51:55 -0500

    Getting Twitter responses back

--- liri.js
+++ liri.js
@@ -1,1 +1,16 @@
-var twitterKeys = require('./keys.js');
+var Twitter = require('twitter');
+var Keys = require('./keys.js');
+
+var userCommand = process.argv;
+
+var twitterClient = new Twitter({
+  consumer_key: Keys.twitterKeys.consumer_key,
+  consumer_secret: Keys.twitterKeys.consumer_secret,
+  access_token_key: Keys.twitterKeys.access_token_key,
+  access_token_secret: Keys.twitterKeys.access_token_secret
+});
+
+twitterClient.get('statuses/user_timeline', 'count=20', function(error, tweets, response) {
+  if (error) throw error;
+  console.log(tweets);
+});
